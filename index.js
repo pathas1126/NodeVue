1da673fa5aae475ddd05294ec6f8c9b38c545fc1
pathas <pathas@naver.com>
2020-02-03 23:33:19 +0900

    Adding socket.io module(connection, join, leave)

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // express 웹 서버를 가져 옴
 const express = require("express"),
   // app : express 초기화
-  app = express();
+  app = express(),
+  util = require("util");
 
 const Pool = require("./pool"),
   Mydb = require("./mydb");
@@ -58,4 +59,50 @@ app.get("/dbtest/:user", (req, res) => {
 // ~ 번 포트에서 실행하도록 함
 const server = app.listen(7000, function() {
   console.log("Express's started on port 7000");
+});
+
+// socket.io
+// 단독으로 사용할 수도 있고 웹서버와도 같이 사용할 수 있는데
+// 웹서버와 같이 사용할 때 이점(DB와 연결한다거나)이 더 많기 때문에
+// 주로 아래와 같이 사용함
+const io = require("socket.io").listen(server, {
+  log: false,
+  // URL이 달라도 채팅서버에 들어올 수 있도록 함
+  origins: "*:*",
+  // 클라이언트와 서버가 서로의 작동 상태를 체크하는 것
+  // default: interval 25초, timeout 60초
+  // handshake: 서로 확인을 하는 것
+  // 한 번만 연결해두면 웹브라우저를 새로고침하거나 껏다 켜도 자동으로 다시 연결되도록 함
+  pingInterval: 3000,
+  pingTimeout: 5000
+});
+
+// 클라이언트가 연결되었을 때
+io.sockets.on("connection", (socket, opt) => {
+  // 클라이언트에 Welcome!! 메세지 전송
+  socket.emit("message", { msg: "Welcome!!" + socket.id });
+
+  // socket의 채팅방 입장(join) 처리
+  socket.on("join", function(roomId, fn) {
+    // 해당 소켓이 채팅방에 들어가게(join) 됨
+    socket.join(roomId, () => {
+      util.log("Join", roomId, Object.keys(socket.rooms));
+    });
+  });
+
+  // socket의 채팅방 퇴장(leave) 처리
+  socket.on("leave", function(roomId, fn) {
+    // 해당 소켓이 채팅방에 들어가게(join) 됨
+    socket.leave(roomId);
+    });
+  });
+
+
+
+  // 클라이언트에서 메세지 전송
+  socket.on("message", (data, fn) => {
+    // Object.keys(매개변수): JSON으로부터 특정 매개변수가 있는 key 값의 value를 찾음
+    // rooms: room(채팅방)에 들어와 있는 socket들의 목록을 배열로 갖고 있음
+    util.log("message>>", data.msg, Object.keys(socket.rooms));
+  });
 });
