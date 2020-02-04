e72e579632e610fb468e337814de2706935f017e
pathas <pathas@naver.com>
2020-02-04 23:41:53 +0900

    Add chat server and client using socket.io

--- index.js
+++ index.js
@@ -69,40 +69,80 @@ const io = require("socket.io").listen(server, {
   log: false,
   // URL이 달라도 채팅서버에 들어올 수 있도록 함
   origins: "*:*",
+
+  // handshake: 서로 확인을 하는 것
   // 클라이언트와 서버가 서로의 작동 상태를 체크하는 것
   // default: interval 25초, timeout 60초
-  // handshake: 서로 확인을 하는 것
   // 한 번만 연결해두면 웹브라우저를 새로고침하거나 껏다 켜도 자동으로 다시 연결되도록 함
   pingInterval: 3000,
   pingTimeout: 5000
 });
 
+// ================ socket.io 서버측 ================
+
 // 클라이언트가 연결되었을 때
 io.sockets.on("connection", (socket, opt) => {
   // 클라이언트에 Welcome!! 메세지 전송
-  socket.emit("message", { msg: "Welcome!!" + socket.id });
+  socket.emit("message", { msg: "Welcome!! " + socket.id });
+
+  // socket handshake의 query(?aaa=123) 출력
+  // query string: 웹 서버에 데이터를 전달할 수 있도록 하는 값
+  util.log("connection>>", socket.id, socket.handshake.query);
 
   // socket의 채팅방 입장(join) 처리
   socket.on("join", function(roomId, fn) {
     // 해당 소켓이 채팅방에 들어가게(join) 됨
     socket.join(roomId, () => {
+      // 채팅방에 입장했을 때 방 id와 연결된 방 목록 출력, 두 개 이상의 방에 들어갈 수 있음
       util.log("Join", roomId, Object.keys(socket.rooms));
+      // 클라이언트에서 콜백 함수가 전달되지 않을 수도 있기 때문에
+      // 항상 fn이 있는 경우에 fn()을 호출하도록 함
+      if (fn) fn();
     });
   });
 
   // socket의 채팅방 퇴장(leave) 처리
   socket.on("leave", function(roomId, fn) {
     // 해당 소켓이 채팅방에 들어가게(join) 됨
-    socket.leave(roomId);
+    socket.leave(roomId, function() {
+      if (fn) fn();
     });
   });
 
+  socket.on("rooms", function(fn) {
+    if (fn) fn(Object.keys(socket.rooms));
+  });
 
-
-  // 클라이언트에서 메세지 전송
+  // 클라이언트에서 메세지 전송(사용자 -> 서버)
+  // data: {room: 'roomid', msg: 'msg 내용...'}
   socket.on("message", (data, fn) => {
     // Object.keys(매개변수): JSON으로부터 특정 매개변수가 있는 key 값의 value를 찾음
     // rooms: room(채팅방)에 들어와 있는 socket들의 목록을 배열로 갖고 있음
     util.log("message>>", data.msg, Object.keys(socket.rooms));
+    // 채팅방 내의 나를 제외한 사람들에게 메세지 전송
+    socket.broadcast
+      .to(data.room)
+      .emit("message", { room: data.room, msg: data.msg });
+    if (fn) fn(data.msg);
+  });
+
+  // 특정 유저에게만 메세지 전송
+  socket.on("message-for-one", (socketid, msg, fn) => {
+    socket.to(socketid).emit("message", { msg: msg });
+  });
+
+  // 연결이 끊기고 있을 때 호출되는 함수
+  socket.on("disconnecting", function(data) {
+    util.log("disconnecting>>", socket.id, Object.keys(socket.rooms));
+    // socket.leave(roomId);
+    // 원래대로라면 연결이 끊겼을 때 방에서 사용자가 떠나게 처리해 주어야 하지만
+    // socket.io가 자동으로 이를 지원해 줌
+  });
+
+  // 연결이 끊긴 뒤에 호출되는 함수
+  socket.on("disconnect", function(data) {
+    // disconnecting 에서는 Object.keys(socket.rooms)로 나오려는 방을 알 수 있지만
+    // disconnect에서는 socket.id 값만 확인 가능
+    util.log("disconnect>>", socket.id, Object.keys(socket.rooms));
   });
 });
